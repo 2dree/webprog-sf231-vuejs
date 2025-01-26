59a74d9a004938285ea3a36204e02846213ae691
2dree <pjco3@student.apc.edu.ph>
2025-01-27 01:32:45 +0800

    added a second component

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -9,18 +9,19 @@
   export default {
     data() {
       return {
-        name: 'Apples',
-        message: 'I like apples'
+        name: 'Mangoes',
+        message: 'I like Mangoes'
       }
     }
   };
   </script>
   
-  <style>  
+  <style>
     #app > div {
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: lightgreen;
-  }</style>
+    background-color: lightyellow;
+  }
+</style>
