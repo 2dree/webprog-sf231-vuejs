8c702ee6017a70adeab0ee1c344204eb8bbcb108
2dree <pjco3@student.apc.edu.ph>
2025-01-27 01:14:32 +0800

    ewa

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,4 +4,12 @@
 <h1>Hello WEBPROG SF231 WORLD!</h1>
 </template>
  
-<style></style>
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
