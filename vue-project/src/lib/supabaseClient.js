6edc0d47127bd3ea56b528cfd9dada6622953107
2dree <pjco3@student.apc.edu.ph>
2025-02-02 15:37:29 +0000

    Query data from the app

---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://gjasxeizusnjcgvzkgjl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqYXN4ZWl6dXNuamNndnprZ2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1MDkwNzMsImV4cCI6MjA1NDA4NTA3M30.1atz1B-K-iPCM7bS9gMA4WF-c73femcUgMZ_qp38KtE')
