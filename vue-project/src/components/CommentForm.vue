9e15af2fd85d8032a3619cf8bd0cb3d0f439e53b
2dree <pjco3@student.apc.edu.ph>
2025-02-10 11:30:14 +0800

    eee

--- vue-project/src/components/CommentForm.vue
+++ vue-project/src/components/CommentForm.vue
@@ -20,15 +20,17 @@
   
   <script setup>
   import { ref } from 'vue';
-  import { supabase } from '../lib/supabaseClient'
-
+  import { createClient } from '@supabase/supabase-js';
   
   const name = ref('');
   const comment = ref('');
   const submissionStatus = ref(null);
   
   // Your Supabase URL and Key - IMPORTANT!
+  
   const tableName = 'comments'; // Name of your Supabase table
+  
+  const supabase = createClient(supabaseUrl, supabaseKey);
   
   async function submitComment() {
     submissionStatus.value = "Submitting...";
