6dfbd990975b38f2c9c669196a1f0573cb10beac
2dree <pjco3@student.apc.edu.ph>
2025-02-12 18:00:51 +0000

    updated

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -6,6 +6,8 @@ import Country from './components/Country.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
+import RestApi from './components/RestApi.vue
+
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
@@ -14,4 +16,5 @@ app.component('country', Country)
 app.component('personal-profile', PersonalProfile)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
+app.component('rest-api', RestApi)
 app.mount('#app')
